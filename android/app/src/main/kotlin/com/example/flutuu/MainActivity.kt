8788e4b3a5958e7c5a64430ab988f6e52b6e3082
bhargavpatel1509 <bhargavkhunt965@gmail.com>
2022-09-14 17:33:41 +0530

    hey

---
+++ android/app/src/main/kotlin/com/example/flutuu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutuu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/examplefrist/flutuu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.examplefrist.flutuu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
